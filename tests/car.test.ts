09070ea34c36eb123fb2077ad6fee88af27b04ad
MarceloMosl <marcelomosl@icloud.com>
2023-07-24 16:22:08 -0300

    test config

--- tests/car.test.ts
+++ tests/car.test.ts
@@ -12,3 +12,26 @@ describe("Should return OK and status 200 when getting all cars", () => {
     expect(resultado.status).toBe(200);
   });
 });
+
+describe("Should return Error when missing a key object", () => {
+  it("testando post/createCars", async () => {
+    const invalidBody = {};
+
+    const resultado = (await api.post("/createCars")).body(invalidBody);
+    expect(resultado.status).toBe(422);
+  });
+});
+
+describe("Should return OK when creating a new Car", () => {
+  it("testando post/createCars", async () => {
+    const validBody = {
+      title: "Prisma",
+      brand: "VW",
+      price: "70.000",
+      age: 2020,
+    };
+
+    const resultado = (await api.post("/createCars")).body(validBody);
+    expect(resultado.status).toBe(200);
+  });
+});
